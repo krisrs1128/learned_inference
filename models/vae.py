82bab986db0ccba57c5f4dca3c8f6378f47b33a1
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-09 13:11:09 -0700

    outlining the vae

--- models/vae.py
+++ models/vae.py
@@ -5,32 +5,38 @@ mostly copied from: https://github.com/rasbt/deeplearning-models/blob/master/pyt
 """
 import torch
 from torch import nn
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class VariationalAutoencoder(torch.nn.Module):
-    def __init__(self, n_input, n_hiddens, n_latent, negative_slope=0.0001):
+    def __init__(self, n_latent=100, negative_slope=0.0001):
         super(VariationalAutoencoder, self).__init__()
 
         ### ENCODER
         self.encoder = nn.Sequential(
-            nn.Linear(n_input, n_hiddens[0]),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=1, stride=1, padding=0),
             nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Linear(n_hiddens[0], n_hiddens[1]),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Linear(n_hiddens[1], n_hiddens[2])
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=4, padding=1),
+            nn.LeakyReLU(negative_slope=negative_slope),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=4, padding=1),
+            nn.LeakyReLU(negative_slope=negative_slope),
         )
 
-        self.z_mean = nn.Linear(n_hiddens[-1], n_latent)
-        self.z_log_var = nn.Linear(n_hiddens[-1], n_latent)
+        self.z_mean = nn.Linear(256 * 6 * 6, n_latent)
+        self.z_log_var = nn.Linear(256 * 6 * 6, n_latent)
 
         ### DECODER
+        self.dec_linear = nn.Linear(n_latent, 256 * 6 * 6)
         self.decoder = nn.Sequential(
-            nn.Linear(n_latent, n_hiddens[2]),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=1, stride=1),
             nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Linear(n_hiddens[2], n_hiddens[1]),
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=4),
             nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Linear(n_hiddens[1], n_hiddens[0]),
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=4),
             nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Linear(n_hiddens[0], n_input)
+            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=1, stride=1),
+            nn.LeakyReLU(negative_slope=negative_slope),
         )
 
     def reparameterize(self, z_mu, z_log_var):
@@ -40,19 +46,36 @@ class VariationalAutoencoder(torch.nn.Module):
         # i.e., std_dev = exp(log(std_dev^2)/2) = exp(log(var)/2)
         return z_mu + eps * torch.exp(z_log_var/2.)
 
-    def encoder(self, x):
+    def encode(self, x):
         x = self.encoder(x)
-        z_mean = self.z_mean(x)
-        z_log_var = self.z_log_var(x)
+        z_mean = self.z_mean(x.view(-1, 256 * 6 * 6))
+        z_log_var = self.z_log_var(x.view(-1, 256 * 6 * 6))
         encoded = self.reparameterize(z_mean, z_log_var)
         return z_mean, z_log_var, encoded
 
-    def decoder(self, z):
-        x = self.decoder(z)
-        decoded = torch.sigmoid(x)
-        return decoded
+    def decode(self, z):
+        x = self.dec_linear(z)
+        x = x.view(-1, 256, 6, 6)
+        x = self.decoder(x)
+        return torch.sigmoid(x)
 
     def forward(self, x):
-        z_mean, z_log_var, encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
+        z_mean, z_log_var, encoded = self.encode(x)
+        decoded = self.decode(encoded)
         return z_mean, z_log_var, encoded, decoded
+
+
+if __name__ == '__main__':
+    import pathlib
+    import os
+    import sim.data as dt
+    from torch.utils.data import DataLoader
+
+    npy_dir = pathlib.Path(os.environ["ROOT_DIR"], "data", "npys")
+    data = dt.CellDataset(npy_dir, dt.RandomCrop(96))
+
+    vae = VariationalAutoencoder()
+
+    loader = DataLoader(data, batch_size=32)
+    for x in loader:
+        z_mean, z_var, encoded, decoded = vae.forward(x)
--- sim/data.py
+++ sim/data.py
@@ -27,12 +27,50 @@ class CellDataset(Dataset):
     Dataset for working with tiffs of cells
     """
 
-    def __init__(self, input_dir):
+    def __init__(self, input_dir, transform=None):
         """Initialize dataset."""
-        self.img_files = pathlib.Path(input_dir).glob("*npy")
+        self.img_files = list(pathlib.Path(input_dir).glob("*npy"))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_files)
+
+    def __getitem__(self, i):
+        img = np.load(self.img_files[i])
+        if self.transform:
+            img = self.transform(img)
+
+        return torch.Tensor(img.transpose(2, 0, 1))
+
+
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, x):
+        h, w = x.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+        return x[top: top + new_h, left: left + new_w]
 
 
 if __name__ == '__main__':
     tiff_dir = pathlib.Path(os.environ["ROOT_DIR"], "data", "tiffs")
     npy_dir = pathlib.Path(os.environ["ROOT_DIR"], "data", "npys")
     convert_dir_numpy(tiff_dir, npy_dir)
+    data = CellDataset(npy_dir, dt.RandomCrop(64))
+    data[0]
