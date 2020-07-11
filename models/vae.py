8ee51e8a874e413c515abac6a3fc553f6081830b
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-10 17:35:54 -0700

    move imports

--- models/vae.py
+++ models/vae.py
@@ -5,6 +5,10 @@ mostly copied from: https://github.com/rasbt/deeplearning-models/blob/master/pyt
 """
 import torch
 from torch import nn
+import pathlib
+import os
+import sim.data as dt
+from torch.utils.data import DataLoader
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class VariationalAutoencoder(torch.nn.Module):
@@ -66,11 +70,6 @@ class VariationalAutoencoder(torch.nn.Module):
 
 
 if __name__ == '__main__':
-    import pathlib
-    import os
-    import sim.data as dt
-    from torch.utils.data import DataLoader
-
     npy_dir = pathlib.Path(os.environ["ROOT_DIR"], "data", "npys")
     data = dt.CellDataset(npy_dir, dt.RandomCrop(96))
 
