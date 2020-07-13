9f9d65e3d27f5a63bfa38ac0f27c62fbf770c6ee
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-13 12:30:19 -0700

    changing parameter names

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,7 @@
 
-Rscript -e "rmarkdown::render('sim/generate.Rmd')"
+source .env
 source venv/bin/activate
+Rscript -e "rmarkdown::render('sim/generate.Rmd')"
 python3 -m sim.data
 python3 -m sim.vae -c conf/train.yaml
-python3 -m sim.features -c conf/train.yaml -m data/runs/vae_20200711/model_140.pt
+python3 -m sim.features -c conf/train.yaml -m data/runs/vae_20200711/model_90.pt
