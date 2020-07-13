4f6e5223175831d94864de697c058a979c75b460
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-13 12:30:00 -0700

    use a different vae implementation

--- models/vae.py
+++ models/vae.py
@@ -6,77 +6,95 @@ mostly copied from: https://github.com/rasbt/deeplearning-models/blob/master/pyt
 import os
 import pathlib
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torch
 import sim.data as dt
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class VariationalAutoencoder(torch.nn.Module):
-    """
-    Convolutional VAE, designed for 96 x 96 inputs
-    """
-    def __init__(self, n_latent=100, negative_slope=0.0001):
-        super(VariationalAutoencoder, self).__init__()
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
 
-        # encoder
+
+class UnFlatten(nn.Module):
+    def forward(self, input, size=1024):
+        return input.view(input.size(0), size, 1, 1)
+
+
+class VAE(nn.Module):
+    def __init__(self, image_channels=3, h_dim=1024, z_dim=32):
+        super(VAE, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=1, stride=1, padding=0),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=4, padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=4, padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope),
+            nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2),
+            nn.ReLU(),
+            Flatten()
         )
 
-        self.z_mean = nn.Linear(256 * 6 * 6, n_latent)
-        self.z_log_var = nn.Linear(256 * 6 * 6, n_latent)
+        self.fc1 = nn.Linear(h_dim, z_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(z_dim, h_dim)
 
-        # decoder
-        self.dec_linear = nn.Linear(n_latent, 256 * 6 * 6)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=1, stride=1),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4, stride=4),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=4),
-            nn.LeakyReLU(negative_slope=negative_slope),
-            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=1, stride=1),
-            nn.LeakyReLU(negative_slope=negative_slope),
+            UnFlatten(),
+            nn.ConvTranspose2d(h_dim, 128, kernel_size=5, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, image_channels, kernel_size=6, stride=2),
+            nn.Sigmoid(),
         )
 
-    def reparameterize(self, z_mu, z_log_var):
-        # Sample epsilon from standard normal distribution
-        eps = torch.randn(z_mu.size(0), z_mu.size(1)).to(device)
-        # note that log(x^2) = 2*log(x); hence divide by 2 to get std_dev
-        # i.e., std_dev = exp(log(std_dev^2)/2) = exp(log(var)/2)
-        return z_mu + eps * torch.exp(z_log_var/2.)
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        eps = torch.randn(*mu.size())
+        return mu + std * eps
+
+    def bottleneck(self, h):
+        mu, logvar = self.fc1(h), self.fc2(h)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
 
     def encode(self, x):
-        x = self.encoder(x)
-        z_mean = self.z_mean(x.view(-1, 256 * 6 * 6))
-        z_log_var = self.z_log_var(x.view(-1, 256 * 6 * 6))
-        encoded = self.reparameterize(z_mean, z_log_var)
-        return z_mean, z_log_var, encoded
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h)
+        return z, mu, logvar
 
     def decode(self, z):
-        x = self.dec_linear(z)
-        x = x.view(-1, 256, 6, 6)
-        x = self.decoder(x)
-        return torch.sigmoid(x)
+        z = self.fc3(z)
+        return self.decoder(z)
 
     def forward(self, x):
-        z_mean, z_log_var, encoded = self.encode(x)
-        decoded = self.decode(encoded)
-        return z_mean, z_log_var, encoded, decoded
+        z, mu, logvar = self.encode(x)
+        z = self.decode(z)
+        return z, mu, logvar
+
+def loss_fn(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x, size_average=False)
+    # BCE = F.mse_loss(recon_x, x, size_average=False)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD, BCE, KLD
 
 
 if __name__ == '__main__':
     npy_dir = pathlib.Path(os.environ["ROOT_DIR"], "data", "npys")
-    data = dt.CellDataset(npy_dir, dt.RandomCrop(96))
+    data = dt.CellDataset(npy_dir, pathlib.Path("data", "Xy.csv"), dt.RandomCrop(64))
     vae = VariationalAutoencoder()
 
     loader = DataLoader(data, batch_size=32)
-    for x in loader:
+    for x, _, _ in loader:
         z_mean, z_var, encoded, decoded = vae.forward(x)
