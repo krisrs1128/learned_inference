e90323589e31480caa5412e9a91932abfe06e60e
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-02 18:57:16 -0500

    permissions issue

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -5,5 +5,5 @@ source .env
 cd learning
 python3 -m bootstrap -c ../conf/train.yaml
 python3 -m train -c ../conf/train.yaml -b ${1}
-tar zcvf data_output_${2}_${1}.tar.gz $DATA_DIR
-mv data_output_${2}_${1}.tar.gz $_CONDOR_SCRATCH_DIR/
+cd $_CONDOR_SCRATCH_DIR/
+tar -zcvf data_output_${2}_${1}.tar.gz $DATA_DIR
