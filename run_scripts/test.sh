b6bfce9b3eedc352faba089b2703d3e60f774121
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-02 10:01:09 -0500

    test is reserved

--- run_scripts/test.sh
+++ run_scripts/test.sh
@@ -4,10 +4,11 @@ source .env
 mkdir $DATA_DIR
 touch $DATA_DIR/test1.out
 echo ${1}
-python3 test.py -b 10
-touch pt1.out
+echo $ROOT_DIR
 cd $ROOT_DIR/learning/
-python3 -m test -b 10
+python3 test_condor.py -b 10
+touch pt1.out
+python3 -m test_condor -b 10
 touch pt2.out
-python3 -m test -b ${1}
+python3 -m test_condor -b ${1}
 touch pt3.out
