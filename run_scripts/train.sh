268391b76f0aa29f6fdd521d76a62b02817b6470
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-02 18:30:49 -0500

    don't store full path

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -5,4 +5,5 @@ source .env
 cd learning
 python3 -m bootstrap -c ../conf/train.yaml
 python3 -m train -c ../conf/train.yaml -b ${1}
-tar zcvf $_CONDOR_SCRATCH_DIR/data_output_${1}_${2}.tar.gz $DATA_DIR
+tar zcvf data_output_${2}_${1}.tar.gz $DATA_DIR
+mv data_output_${2}_${1}.tar.gz $_CONDOR_SCRATCH_DIR/
