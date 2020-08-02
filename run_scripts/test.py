8337241321e899d42c371781deefa74a594a1c8e
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-02 09:24:33 -0500

    wrong rootdir

--- run_scripts/test.py
+++ run_scripts/test.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--boot", type=int, help="bootstrap iteration")
     args = parser.parse_args()
+    with open(f"ptf{args.boot}.out", "w") as f:
+        f.writelines(f"test {args.boot}")
     print("testing...")
     print(args.boot)
