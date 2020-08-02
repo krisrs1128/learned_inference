5df35895839761e508fff4b9ae43e9c642beefd8
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-01 21:02:19 -0500

    simple test for python

--- run_scripts/test.sh
+++ run_scripts/test.sh
@@ -3,6 +3,4 @@ cd /home/kris/
 source .env
 mkdir $DATA_DIR
 touch $DATA_DIR/test1.out
-touch $_CONDOR_SCRATCH_DIR/test2.out
-mkdir $_CONDOR_SCRATCH_DIR/data
-touch $_CONDOR_SCRATCH_DIR/data/test3.out
+python3 -c 'print("Hi")'
