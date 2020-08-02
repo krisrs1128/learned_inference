d897baa70f9fbb042f46de4f70a845d9cb3d1051
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-02 11:38:47 -0500

    int -> str

--- run_scripts/test_condor.py
+++ run_scripts/test_condor.py
@@ -4,7 +4,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--boot", type=int, help="bootstrap iteration")
-    parser.add_argument("-f", "--out_dir", type=int, help="output directory")
+    parser.add_argument("-f", "--out_dir", type=str, help="output directory")
     args = parser.parse_args()
     with open(f"{args.out_dir}/ptf{args.boot}.out", "w") as f:
         f.writelines(f"test {args.boot}")
