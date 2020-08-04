3cd061de66e79b647bb907742b854be0bf608314
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-04 15:37:52 -0500

    automatic script generation

--- run_scripts/fine_tuning/train_35.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar -zxvf $_CONDOR_SCRATCH_DIR/data.tar.gz
-cd /home/kris/
-source .env
-cd learning
-python3 -m train -c ../conf/fine_tuning/start_35.yaml -b ${1}
-cd $_CONDOR_SCRATCH_DIR/
-tar -zcvf data_output_${2}_${1}.tar.gz $DATA_DIR/runs/
