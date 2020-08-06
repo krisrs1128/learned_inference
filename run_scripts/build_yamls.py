319c2145615b6f9e50186b647d693f14e1c1b32f
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-06 15:42:42 -0500

    updating yaml

--- run_scripts/build_yamls.py
+++ run_scripts/build_yamls.py
@@ -12,7 +12,20 @@ def fine_tune_yaml(original_yaml, out_dir, start_epoch=0):
     conf["train"]["checkpoint"] = str(pathlib.Path("runs", "vae_no_boot", "None", f"model_{start_epoch}.pt"))
     conf["train"]["n_epochs"] = conf["train"]["n_epochs"] - start_epoch - 1
 
+    pathlib.Path(out_dir).mkdir(exist_ok=True)
     with open(pathlib.Path(out_dir, f"start_{start_epoch}.yaml"), "w") as f:
+        yaml.dump(conf, f, default_flow_style=False)
+
+
+def coreset_yaml(original_yaml, out_dir, size=100, n_epochs=1000):
+    conf = yaml.safe_load(open(original_yaml, "r"))
+    conf["organization"]["out_dir"] = str(pathlib.Path("runs", f"vae_coreset_{size}"))
+    conf["organization"]["features_dir"] = str(pathlib.Path("features", f"vae_coreset_{size}"))
+    conf["train"]["checkpoint"] = str(pathlib.Path("runs", "vae_no_boot", "None", f"model_70.pt"))
+    conf["train"]["n_epochs"] = n_epochs
+
+    pathlib.Path(out_dir).mkdir(exist_ok=True)
+    with open(pathlib.Path(out_dir, f"coreset_{size}.yaml"), "w") as f:
         yaml.dump(conf, f, default_flow_style=False)
 
 
@@ -42,4 +55,8 @@ def cluster_submit(out_dir, conf_path="train", output_name="cluster.submit"):
 if __name__ == '__main__':
         for epoch in range(0, 60, 10):
             fine_tune_yaml("../conf/train_boot.yaml", "../conf/fine_tuning/", epoch)
-            cluster_submit("../run_scripts/", f"../conf/fine_tuning/start_{epoch}.yaml", f"cluster_{epoch}.submit")
+            cluster_submit("../run_scripts/", f"../conf/fine_tuning/start_{epoch}.yaml", f"tune_{epoch}.submit")
+
+        for size in [250, 500, 1000, 2000]:
+            coreset_yaml("../conf/train_boot.yaml", "../conf/coreset/", size)
+            cluster_submit("../run_scripts/", f"../conf/coreset_{size}.yaml", f"cluster_{epoch}.submit")
