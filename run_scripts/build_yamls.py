2553a50a4374d94ece7a34c8f6d9f6901495daf3
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-05 21:18:36 -0500

    modifying features output name

--- run_scripts/build_yamls.py
+++ run_scripts/build_yamls.py
@@ -8,6 +8,7 @@ import yaml
 def fine_tune_yaml(original_yaml, out_dir, start_epoch=0):
     conf = yaml.safe_load(open(original_yaml, "r"))
     conf["organization"]["out_dir"] = str(pathlib.Path("runs", f"vae_tune_{start_epoch}"))
+    conf["organization"]["features_dir"] = str(pathlib.Path("features", f"vae_tune_{start_epoch}"))
     conf["train"]["checkpoint"] = str(pathlib.Path("runs", "vae_no_boot", "None", f"model_{start_epoch}.pt"))
     conf["train"]["n_epochs"] = conf["train"]["n_epochs"] - start_epoch - 1
 
@@ -29,8 +30,14 @@ def fine_tune_sh(out_dir, start_epoch):
     with open(pathlib.Path(out_dir, f"train_{start_epoch}.sh"), "w") as f:
         f.write('\n'.join(lines))
 
+def coreset_yaml():
+    pass
+
+def coreset_sh():
+    pass
+
 
 if __name__ == '__main__':
         for epoch in range(0, 60, 10):
-                     fine_tune_yaml("../conf/train.yaml", "../conf/fine_tuning/", epoch)
-                     fine_tune_sh("../run_scripts/fine_tuning", epoch)
+            fine_tune_yaml("../conf/train.yaml", "../conf/fine_tuning/", epoch)
+            fine_tune_sh("../run_scripts/fine_tuning", epoch)
