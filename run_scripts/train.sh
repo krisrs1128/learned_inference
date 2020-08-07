a3c60b45bbb7242d0f4fbe5c539a331275955e02
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-07 08:28:43 -0500

    remove again

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-ls $HOME
 cd $HOME
-pwd
 source .env
-ls $ROOT_DIR
-ls $DATA_DIR
 cd $ROOT_DIR/learning
-ls
-pwd
 python3 -m bootstrap -c ${1}
 python3 -m train -c ${1} -b ${2}
 python3 -m features -c ${1}
