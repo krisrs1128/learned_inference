0eba1475fd107e3093f56388ea74cc1366b722e3
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-07 10:57:01 -0500

    printing features

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -3,10 +3,12 @@ tar -zxvf $_CONDOR_SCRATCH_DIR/data.tar.gz
 cd $HOME
 source .env
 cd $ROOT_DIR/learning
+echo ${1}
 python3 -m bootstrap -c ${1}
 python3 -m train -c ${1} -b ${2}
 python3 -m features -c ${1}
 
 cd $_CONDOR_SCRATCH_DIR/
 ls $DATA_DIR
+ls $DATA_DIR/features/
 tar -zcvf data_output_${3}_${2}.tar.gz $DATA_DIR/features/
