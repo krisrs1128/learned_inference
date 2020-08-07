2843bd9bbf530fcfdfb138ee2719b4b638c31365
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-07 16:55:19 -0500

    change cluster submit image

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -8,6 +8,4 @@ python3 -m train -c ${1} -b ${2}
 python3 -m features -c ${1}
 
 cd $_CONDOR_SCRATCH_DIR/
-ls $DATA_DIR
-ls $DATA_DIR/features/
 tar -zcvf data_output_${3}_${2}.tar.gz $DATA_DIR/features/
