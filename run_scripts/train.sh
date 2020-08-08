cf9322b9819a3cedffab6505e8ebb880ebda4df4
Kris Sankaran <sankaran.kris@gmail.com>
2020-08-07 22:45:49 -0500

    print results of each command

--- run_scripts/train.sh
+++ run_scripts/train.sh
@@ -10,8 +10,11 @@ echo ${3}
 ls -R
 pwd
 python3 -m bootstrap -c ${1}
+ls -R $DATA_DIR
 python3 -m train -c ${1} -b ${2}
+ls -R $DATA_DIR
 python3 -m features -c ${1}
+ls -R $DATA_DIR
 
 cd $_CONDOR_SCRATCH_DIR/
 tar -zcvf data_output_${3}_${2}.tar.gz $DATA_DIR/features/
