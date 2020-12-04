5f964256e7cbc15f67e30dda536f4472e68ed010
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-03 19:08:46 -0600

    functions to support stability selection

---
+++ inference/R/stability.R
@@ -0,0 +1,24 @@
+
+#' @importFrom glmnet glmnet
+#' @export
+stability_selection <- function(X, y, B = 1000) {
+  n <- nrow(X)
+  p <- ncol(X)
+  coef_paths <- array(dim = c(p, n_lambda, B))
+  for (b in seq_len(B)) {
+    ix <- sample(seq_len(n), n / 2, replace = FALSE)
+    fit <- glmnet(X[ix, ], y[ix])
+    coef_paths[, , b] <- coef(fit)
+  }
+
+  Pi <- apply(coef_paths, 3, function(z) abs(z) > 0)
+  list(Pi = Pi, coef_paths = coef_paths)
+}
+
+#' @importFrom reshape2 melt
+#' @export
+melt_stability <- function(res) {
+  mpi <- melt(res$Pi, varnames = c("j", "lambda"))
+  mcoef_paths <- melt(res$coef_paths, varnames = c("j", "lambda", "b"))
+  list(Pi = mpi, coef_paths = mcoef_paths)
+}
