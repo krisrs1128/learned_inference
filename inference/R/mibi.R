82edbbcc2a728dd1e992f9bd241a2079b4940d97
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-21 11:29:25 -0600

    remove whitespace

--- inference/R/mibi.R
+++ inference/R/mibi.R
@@ -6,17 +6,17 @@ load_mibi <- function(data_dir, n_paths = NULL) {
     "*.tiff",
     full.names = T
   )
-  
+
   if (is.null(n_paths)) {
     n_paths <- length(tiff_paths)
   }
-  
+
   # create a cell-type column
   colData(exper)$cell_type <- colData(exper)$tumor_group
   immune_ix <- colData(exper)$cell_type == "Immune"
   colData(exper)$cell_type[immune_ix] <- colData(exper)$immune_group[immune_ix]
   colData(exper)$cell_type <- forcats::fct_lump_n(colData(exper)$cell_type, 10)
-  
+
   # subset to those samples with images
   tiff_paths <- tiff_paths[1:n_paths]
   sample_names <- str_extract(tiff_paths, "[0-9]+")
@@ -33,20 +33,20 @@ spatial_subsample <- function(tiff_paths, exper, qsize=500) {
     r <- raster(tiff_paths[[i]])
     ims[[tiff_paths[[i]]]] <- crop(r, extent(1, qsize, 1, qsize))
   }
-  
+
   im_ids <- str_extract(tiff_paths, "[0-9]+")
   cur_cells <- map2_dfr(
-    ims, im_ids, 
+    ims, im_ids,
     ~ data.frame(SampleID = .y, cellLabelInImage = unique(as.vector(.x)))
     ) %>%
     unite(sample_by_cell, SampleID, cellLabelInImage, remove = F)
-  
+
   scell <- colData(exper) %>%
     as.data.frame() %>%
     dplyr::select(SampleID, cellLabelInImage) %>%
     unite(sample_by_cell, SampleID, cellLabelInImage) %>%
     .[["sample_by_cell"]]
-  
+
   list(
     ims = ims,
     exper = exper[, scell %in% cur_cells$sample_by_cell]
@@ -59,13 +59,13 @@ subset_exper <- function(id, r, exper) {
     dplyr::select(SampleID, cellLabelInImage) %>%
     unite(sample_by_cell, SampleID, cellLabelInImage) %>%
     pull("sample_by_cell")
-  
+
   sample_by_cell <- data.frame(
-    SampleID = id, 
+    SampleID = id,
     cellLabelInImage = unique(as.vector(r))
   ) %>%
   unite(sample_by_cell, SampleID, cellLabelInImage, remove = F)
-  
+
   exper[, scell %in% sample_by_cell$sample_by_cell]
 }
 
@@ -78,48 +78,47 @@ unwrap_channels <- function(r, r_cells) {
       filter(cell_type == cell_types[i]) %>%
       pull(cellLabelInImage) %>%
       unique()
-    
+
     r_mat[,, i] <- 1 * as.matrix(r %in% cur_cells)
   }
-  r_mat 
+  r_mat
 }
 
 extract_patch <- function(r, w, h, r_cells, response, qsize = 128) {
   r <- crop(r, extent(w, w + qsize, h, h + qsize))
   cur_cells <- colData(exper)$cellLabelInImage %in% unique(as.vector(r))
   r <- unwrap_channels(r, r_cells)
-  
+
   y <- mean(assay(exper)[response, cur_cells] > 0) # proportion of active cells
   list(x = r, y = y)
 }
 
-extract_patches <- function(tiff_paths, exper, response = "PD1", qsize = 128, 
+extract_patches <- function(tiff_paths, exper, response = "PD1", qsize = 128,
                             out_dir = ".") {
   np <- reticulate::import("numpy")
   im_ids <- str_extract(tiff_paths, "[0-9]+")
   y_path <- file.path(out_dir, "y.csv")
-  
+
   j <- 1
   for (i in seq_along(tiff_paths)) {
     print(i)
     r <- raster(tiff_paths[i])
     ix_start <- seq(1, ncol(r), by = qsize)
     r_cells <- subset_exper(im_ids[i], r, exper)
-    
+
     for (w in seq_along(ix_start)) {
       for (h in seq_along(ix_start)) {
         patch <- extract_patch(r, ix_start[w], ix_start[h], r_cells, response, qsize)
-        
+
         # write results
         npy_path <- file.path(out_dir, sprintf("patch_%s_%s-%s.npy", i, w, h))
         np$save(npy_path, patch$x)
-  #      writeRaster(patch$x, file.path(raster_path), format="GTiff", overwrite=TRUE)
-        y <- data.frame(path = tiff_paths[i], i = i, w = w, h = h, y = patch$y)       
+        y <- data.frame(path = tiff_paths[i], i = i, w = w, h = h, y = patch$y)
         write.table(y, y_path, sep = ",", col.names = !file.exists(y_path), append = T)
-        
+
         j <- j + 1
       }
     }
   }
-  
+
 }
