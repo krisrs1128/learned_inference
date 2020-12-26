2ab1f148995e7e3c75ce969592b96a9d7cdaf147
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-26 13:31:10 -0600

    shorten to Path

--- stability/stability/data.py
+++ stability/stability/data.py
@@ -2,7 +2,7 @@
 Utilities for working with multichannel Tiffs
 
 """
-import pathlib
+from pathlib import Path
 import random
 import os
 import torch
@@ -19,10 +19,10 @@ def convert_dir_numpy(input_dir, output_dir):
     Wrap tiff_to_numpy over an entire directory
     """
     os.makedirs(output_dir, exist_ok=True)
-    paths = list(pathlib.Path(input_dir).glob("*.tif"))
+    paths = list(Path(input_dir).glob("*.tif"))
     for i, path in enumerate(paths):
-        out_name = pathlib.Path(path).stem + ".npy"
-        tiff_to_numpy(path, pathlib.Path(output_dir, out_name))
+        out_name = Path(path).stem + ".npy"
+        tiff_to_numpy(path, Path(output_dir, out_name))
 
 
 def save_pngs(input_dir, output_dir):
@@ -30,11 +30,11 @@ def save_pngs(input_dir, output_dir):
     Save arrays as pngs, for easier viewing
     """
     os.makedirs(output_dir, exist_ok=True)
-    paths = list(pathlib.Path(input_dir).glob("*.npy"))
+    paths = list(Path(input_dir).glob("*.npy"))
     for i, path in enumerate(paths):
-        out_name = pathlib.Path(path).stem + ".png"
+        out_name = Path(path).stem + ".png"
         im = Image.fromarray((255 * np.load(path)).astype(np.uint8))
-        im.save(pathlib.Path(output_dir, out_name))
+        im.save(Path(output_dir, out_name))
 
 
 def random_split(ids, split_ratio):
@@ -67,14 +67,14 @@ class CellDataset(Dataset):
     """
     Dataset for working with simulated cell images
     """
-    def __init__(self, img_paths, xy_path=None, root=pathlib.Path(".")):
+    def __init__(self, img_paths, xy_path=None, root=Path(".")):
         """Initialize dataset."""
         self.img_paths = img_paths
         self.root = root
 
         # default xy values
         if xy_path:
-            self.xy = pd.read_csv(xy_path, index_col="path")
+            self.xy = pd.read_csv(root / xy_path, index_col="path")
         else:
             self.xy = {"y": np.zeros(len(img_paths))}
 
@@ -85,21 +85,3 @@ class CellDataset(Dataset):
         img = np.load(self.root / self.img_paths[i])
         y = self.xy.loc[self.img_paths[i], "y"]
         return torch.Tensor(img.transpose(2, 0, 1)), torch.Tensor([y])
-
-
-class MIBIDataset(Dataset):
-    """
-    Dataset for working with real MIBI-ToF data
-    """
-    def __init__(self, y_path, data_dir="."):
-        self.y = pd.read_csv(y_path)
-        self.data_dir = data_dir
-
-    def __len__(self):
-        return len(self.y)
-
-    def __getitem__(self, ix):
-        _, i, w, h, yi = self.y.iloc[ix]
-        xi = np.load(self.data_dir / "train" / f"patch_{i}_{w}-{h}.npy")
-        xi = np.transpose(xi, (2, 0, 1))
-        return torch.Tensor(xi), torch.Tensor([yi])
