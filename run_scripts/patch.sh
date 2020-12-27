6e3d325010d75bf037f31a1a18ad137f7a5bd273
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-27 12:19:13 -0600

    output still needs to be 0 indexed (for condor)

--- run_scripts/patch.sh
+++ run_scripts/patch.sh
@@ -5,12 +5,11 @@ tar -zxvf stability_data.tar.gz
 git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 
-# prepare patches
+# prepare patches, taking care of R's 1 indexing
 export B=${1}
 ((B++))
-echo ${B}
 Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/prepare_mibi.Rmd', params = list(j = ${B}, data_dir='../../../stability_data/tnbc/'))"
 mv stability_data/tnbc/tiles/y.csv stability_data/tnbc/tiles/y_${B}.csv
 cd stability_data/tnbc/
-tar -zcvf tiles_${B}.tar.gz tiles/
-mv tiles_${B}.tar.gz $_CONDOR_SCRATCH_DIR/
+tar -zcvf tiles_${1}.tar.gz tiles/
+mv tiles_${1}.tar.gz $_CONDOR_SCRATCH_DIR/
