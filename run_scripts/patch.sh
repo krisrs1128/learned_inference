7e2f5d63a5c08302faf20e0895952cba4264be18
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-27 11:42:07 -0600

    perhaps just an indexing problem

--- run_scripts/patch.sh
+++ run_scripts/patch.sh
@@ -6,8 +6,10 @@ git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 
 # prepare patches
-Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/prepare_mibi.Rmd', params = list(j = ${1}, data_dir='../../../stability_data/tnbc/'))"
-mv stability_data/tnbc/tiles/y.csv stability_data/tnbc/tiles/y_${1}.csv
+export B=((${1}++))
+echo ${B}
+Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/prepare_mibi.Rmd', params = list(j = ${B}, data_dir='../../../stability_data/tnbc/'))"
+mv stability_data/tnbc/tiles/y.csv stability_data/tnbc/tiles/y_${B}.csv
 cd stability_data/tnbc/
-tar -zcvf tiles_${1}.tar.gz tiles/
-mv tiles_${1}.tar.gz $_CONDOR_SCRATCH_DIR/
+tar -zcvf tiles_${B}.tar.gz tiles/
+mv tiles_${B}.tar.gz $_CONDOR_SCRATCH_DIR/
