c4af568b5ba6b235f8585041bcb084654eaeb69f
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-26 20:26:51 -0600

    inferences -> inference

--- run_scripts/patch.sh
+++ run_scripts/patch.sh
@@ -6,7 +6,7 @@ git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 
 # prepare patches
-Rscript -e "rmarkdown::render('learned_inference/inferences/vignettes/prepare_mibi.Rmd', params = list(j = ${1}, data_dir='../../../stability_data/tnbc/'))"
+Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/prepare_mibi.Rmd', params = list(j = ${1}, data_dir='../../../stability_data/tnbc/'))"
 mv stability_data/tnbc/tiles/y.csv stability_data/tnbc/tiles/y_${1}.csv
 cd stability_data/tnbc/
 tar -zcvf tiles_${1}.tar.gz tiles/
