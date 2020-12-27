926356a20fbb684ec94c42910c00f836ffbf89ff
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-27 11:51:25 -0600

    two steps for increment

--- run_scripts/patch.sh
+++ run_scripts/patch.sh
@@ -6,7 +6,8 @@ git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 
 # prepare patches
-export B=((${1}++))
+export B=${1}
+((B++))
 echo ${B}
 Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/prepare_mibi.Rmd', params = list(j = ${B}, data_dir='../../../stability_data/tnbc/'))"
 mv stability_data/tnbc/tiles/y.csv stability_data/tnbc/tiles/y_${B}.csv
