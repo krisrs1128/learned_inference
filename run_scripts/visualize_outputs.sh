08cd81f27779973ad3ac3e9ffa7ba722d391152f
Kris Sankaran <sankaran.kris@gmail.com>
2020-12-30 22:56:52 -0600

    more data needed for plot

--- run_scripts/visualize_outputs.sh
+++ run_scripts/visualize_outputs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+tar -zxvf stability_data.tar.gz
 tar -zxvf stability_outputs.tar.gz
 
 git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
-Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures'))"
+Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_Dir = '../../../stability_data/'))"
 
 tar -zcvf figures_$PREFIX_$SCA.tar.gz figures/
