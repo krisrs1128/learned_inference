606e3ea04f6570a989602d0bb04a005330d42ceb
Kris Sankaran <sankaran.kris@gmail.com>
2021-01-01 11:42:49 -0600

    go straight to tiles dir

--- run_scripts/visualize_outputs.sh
+++ run_scripts/visualize_outputs.sh
@@ -5,6 +5,6 @@ tar -zxvf stability_outputs.tar.gz
 
 git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
-Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_Dir = '../../../stability_data/'))"
+Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_dir = '../../../stability_data/'))"
 
 tar -zcvf figures_$PREFIX_$LAYER_$SCA.tar.gz figures/
