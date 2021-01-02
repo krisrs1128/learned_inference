2c8b147b308636b0b5cffce218a51a37e00ae3ad
Kris Sankaran <sankaran.kris@gmail.com>
2021-01-02 13:04:56 -0600

    add brackets

--- run_scripts/visualize_outputs.sh
+++ run_scripts/visualize_outputs.sh
@@ -7,4 +7,4 @@ git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_dir = '../../../stability_data/'))"
 
-tar -zcvf figures_$PREFIX_$LAYER_$SCA.tar.gz figures/
+tar -zcvf figures_${PREFIX}_${LAYER}_${SCA}.tar.gz figures/
