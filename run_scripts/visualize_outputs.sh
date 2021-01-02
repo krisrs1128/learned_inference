78e431b0142865fbb5dc321ce138f8c6cf8bdf3b
Kris Sankaran <sankaran.kris@gmail.com>
2021-01-02 13:07:45 -0600

    see where it is

--- run_scripts/visualize_outputs.sh
+++ run_scripts/visualize_outputs.sh
@@ -7,4 +7,7 @@ git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
 Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../stability_outputs/', layer = '${LAYER}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_dir = '../../../stability_data/'))"
 
+ls
+ls figures/
+
 tar -zcvf figures_${PREFIX}_${LAYER}_${SCA}.tar.gz figures/
