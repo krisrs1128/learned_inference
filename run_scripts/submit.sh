5853a9d809e2e0201210998b443a235134427bfa
Kris Sankaran <sankaran.kris@gmail.com>
2021-01-03 21:55:50 -0600

    cluster -> train

--- run_scripts/submit.sh
+++ run_scripts/submit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-bash cluster.submit conf/vae15.yaml
-bash cluster.submit conf/vae50.yaml
-bash cluster.submit conf/vae90.yaml
-bash cluster.submit conf/cnn15.yaml
-bash cluster.submit conf/cnn50.yaml
-bash cluster.submit conf/cnn90.yaml
-bash cluster.submit conf/rcf15.yaml
-bash cluster.submit conf/rcf50.yaml
-bash cluster.submit conf/rcf90.yaml
+bash train.submit conf/vae15.yaml
+bash train.submit conf/vae50.yaml
+bash train.submit conf/vae90.yaml
+bash train.submit conf/cnn15.yaml
+bash train.submit conf/cnn50.yaml
+bash train.submit conf/cnn90.yaml
+bash train.submit conf/rcf15.yaml
+bash train.submit conf/rcf50.yaml
+bash train.submit conf/rcf90.yaml
