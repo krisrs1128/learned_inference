9a4a73fb3ce73b393ef507597ad0ecca74c3a946
Kris Sankaran <sankaran.kris@gmail.com>
2021-01-06 11:07:35 -0600

    save an image with all the results

--- run_scripts/visualize_outputs.sh
+++ run_scripts/visualize_outputs.sh
@@ -5,6 +5,6 @@ tar -zxvf stability_outputs.tar.gz
 
 git clone https://github.com/krisrs1128/learned_inference.git
 source learned_inference/.env
-Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../tnbc_outputs/', basename = '${BASENAME}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_dir = '../../../stability_data/', channels=c(1, 2, 3)))"
+Rscript -e "rmarkdown::render('learned_inference/inference/vignettes/stability.Rmd', params = list(data_dir = '../../../tnbc_outputs/', basename = '${BASENAME}', model_prefix = '${PREFIX}', sca = ${SCA}, save_dir = '../../../figures', tiles_dir = '../../../stability_data/', channels=c(3, 4, 6)))"
 
 tar -zcvf figures_${PREFIX}_${BASENAME}_${SCA}.tar.gz figures/
