b9f5b5773f9ffd0187d620f80fc328732fc81306
Kris Sankaran <sankaran.kris@gmail.com>
2021-12-10 17:13:14 -0600

    updating env

--- run_scripts/submit.sh
+++ run_scripts/submit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-bash train.submit conf/vae15.yaml
-bash train.submit conf/vae50.yaml
-bash train.submit conf/vae90.yaml
-bash train.submit conf/cnn15.yaml
-bash train.submit conf/cnn50.yaml
-bash train.submit conf/cnn90.yaml
-bash train.submit conf/rcf15.yaml
-bash train.submit conf/rcf50.yaml
-bash train.submit conf/rcf90.yaml
+#bash train.submit conf/vae15.yaml
+#bash train.submit conf/vae50.yaml
+#bash train.submit conf/vae90.yaml
+bash train.submit conf/cnn-k32.yaml
+#bash train.submit conf/cnn50.yaml
+#bash train.submit conf/cnn90.yaml
+#bash train.submit conf/rcf15.yaml
+#bash train.submit conf/rcf50.yaml
+#bash train.submit conf/rcf90.yaml
